706be3259f43f81881ae579e363db93bacc9a3a1
wzh <wzh191920@sina.com>
2017-12-15 12:31:08 +0800

    PlugInTest

---
+++ PlugInTest/PlugInTest.cpp
@@ -0,0 +1,30 @@
+// PlugInTest.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "PlugInTest.h"
+#include "MyDataPlugIn.h"
+
+DLL_API bool InitCallBack()
+{
+	//程序加载时，初始化写这里，返回值决定是否加载成功
+	return true;
+}
+DLL_API bool WriteNewDataCallBack(int table_id, int point_id, time_t t, const char* data)
+{//写入实时数据前的处理写这里，返回值决定是否写入数据库
+
+	//功能，如果是表id为1的数据，如果第一个基本类型（即，仪表1）的数是奇数，则不写入数据库
+	if (table_id == 1)
+	{
+		SerializerNoBuf io(data);
+		int i = 0;
+		PlugInReadInt(&io, &i);
+		if (i & 1)
+			return false;
+	}
+	return true;
+}
+DLL_API void CloseCallBack()
+{
+	//程序退出时，资源释放的处理写这里
+}
---
+++ PlugInTest/PlugInTest.h
@@ -0,0 +1,24 @@
+#pragma once
+//MyData插件，函数名可以自定义，但是参数和返回值参照PlugIn.h中的要求，不需要三个回调函数都实现
+//插件的配置参照：插件使用说明.txt
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DLL_API extern "C" __declspec(dllexport)
+
+//插件加载时的初始化回调
+//返回值为true，插件加载成功，否则加载失败
+DLL_API bool InitCallBack();
+
+//实时数据写入前的回调，可以用于判断是否符合写入条件，也可以把数据发往其他程序
+//返回值为true，把数据写入数据库，否则丢弃数据
+DLL_API bool WriteNewDataCallBack(int table_id, int point_id, time_t t, const char* data);
+
+//程序结束时，插件释放资源的回调函数
+DLL_API void CloseCallBack();
+
+#ifdef __cplusplus
+}
+#endif
